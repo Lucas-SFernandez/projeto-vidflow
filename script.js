5fb21d5d9ae3d67ea138ca388b6de770daeef2f9
Lucas-SFernandez <127568078+Lucas-SFernandez@users.noreply.github.com>
2024-09-05 15:22:55 -0300

    projeto de plataforma similar ao YouTube

---
+++ script.js
@@ -0,0 +1,65 @@
+//Barra de pesquisa
+const barraDePesquisa = document.querySelector('.pesquisar__input');
+
+barraDePesquisa.addEventListener('input', filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro =  barraDePesquisa.value.toLowerCase();
+
+    videos.forEach((video) => {
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+}
+
+//Filtro de categorias
+const botaoCategoria = document.querySelectorAll('.superior__item');
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute('name');
+    botao.addEventListener('click', () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro){
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = filtro.toLowerCase();
+
+    videos.forEach((video) => {
+        const categoria = video.querySelector('.categoria').textContent.toLowerCase();
+
+        video.style.display = !categoria.includes(valorFiltro) && valorFiltro != 'tudo' ? 'none' : 'block';
+    });
+
+}
+
+//Buscando e mostrando os vídeos na tela
+const containerVideos = document.querySelector('.videos__container');
+
+
+async function buscarMostrarVideos() {
+    try{
+    const buscar = await fetch('http://localhost:3000/videos');
+    const videos = await buscar.json();
+
+    videos.forEach((video) => {
+        containerVideos.innerHTML += `
+    <li class="videos__item">
+    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+    
+    <div class='descricao-video'>
+    <img class='img-canal' src='${video.imagem} alt='Logo do canal'/>
+    <h3 class='titulo-video'>${video.titulo}</h3>
+    <p class='titulo-canal'>${video.descricao}</p>
+    <p class='categoria' hidden>${video.categoria}</p>
+    </div>
+    </li>
+    `;
+    })
+} catch(err){
+            containerVideos.innerHTML = `<p style="color: red; font-size: 18px;"> Houve um erro ao carregar os vídeos; ${err} </br> Pro favor tente mais tarde</p>`;
+        }
+}
+
+buscarMostrarVideos();
